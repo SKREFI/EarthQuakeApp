db68046db0b6d17fceddfa3524c587428723096a
SKREFI <srazvan43@yahoo.com>
2018-04-30 17:41:13 +0300

    messege

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,16 +1,21 @@
 package com.example.android.quakereport;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeAdapter extends ArrayList<Earthquake> {
+
+    public EarthquakeAdapter(Context context, List<Earthquake> earthquakes){
+        super(context,0,earthquakes);
+    }
 
     public static class ViewHolder{
         TextView tvMg;
@@ -30,7 +35,7 @@ public class EarthquakeAdapter extends ArrayList<Earthquake> {
         ViewHolder viewHolder;
 
         if(convertView==null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, parent, false);
             viewHolder = new ViewHolder(convertView);
             convertView.setTag(viewHolder);
         }
